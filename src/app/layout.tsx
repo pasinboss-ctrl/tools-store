4d29e27987af7fa91199ea5c8fc0696eaee07bdc
pasinboss-ctrl <pasin.boss@gmail.com>
2025-10-23 12:39:20 +0700

    Fix: images.ts and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Noto_Sans_Thai } from "next/font/google";
+//import { Noto_Sans_Thai } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-const noto = Noto_Sans_Thai({
+/*const noto = Noto_Sans_Thai({
   subsets: ["thai"],
   weight: ["300","400","500","700"],
   variable: "--font-noto",
 });
+*/
 export const metadata: Metadata = {
   title: "HIGH PRO",
   description: "อุปกรณ์ช่างครบ จบที่เดียว",
--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,5 +1,7 @@
 // src/sanity/lib/image.ts
 import imageUrlBuilder from "@sanity/image-url";
 import { sanity } from "./client";
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
+
 const builder = imageUrlBuilder(sanity);
-export const urlFor = (source: any) => builder.image(source);
+export const urlFor = (source: SanityImageSource) => builder.image(source);
